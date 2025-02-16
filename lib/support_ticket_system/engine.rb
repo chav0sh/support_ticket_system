4e0ffbddca574c7a4fb2a28afb8b5bb2aff53768
Chavosh Ramazanian <chavosh.ramazanian@buzzjective.com>
2025-02-17 03:28:36 +0330

    fixed generators to create files in the correct path

--- lib/support_ticket_system/engine.rb
+++ lib/support_ticket_system/engine.rb
@@ -1,5 +1,21 @@
 module SupportTicketSystem
   class Engine < ::Rails::Engine
     isolate_namespace SupportTicketSystem
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+
+      # Ensure generated factories go into test/dummy/spec/factories
+      g.fixture_replacement :factory_bot, dir: 'test/dummy/spec/factories'
+
+      # ORM Configuration
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
