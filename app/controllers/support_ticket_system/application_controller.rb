4011ee3f0f6d79390c7ca573075d57058a190b90
Chavosh Ramazanian <chavosh.ramazanian@buzzjective.com>
2025-02-17 09:46:48 +0330

    test if linter works 2

--- app/controllers/support_ticket_system/application_controller.rb
+++ app/controllers/support_ticket_system/application_controller.rb
@@ -1,5 +1,13 @@
 module SupportTicketSystem
   class ApplicationController < ActionController::Base
-    # protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
+
+
+
+
+
+
+              
+                 
   end
 end
